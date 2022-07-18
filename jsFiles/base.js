bc2d6044f1198185d766349af6769a9c9622c104
jangal mangal <jangal@mangal>
2022-07-18 22:16:06 +0530

    logic build correctly

--- jsFiles/base.js
+++ jsFiles/base.js
@@ -1,1 +1,53 @@
-console.log("ATTACHED");
+var ii=window.localStorage;
+
+for(var pp=0;pp<localStorage.length;pp++){
+        
+var valuee=localStorage.getItem(`NOTE-${pp}`);
+
+var template=`<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title"></h5>
+  <p class="card-text">${valuee}</p>
+ 
+</div>
+</div>`
+
+    
+var ourContainer=document.getElementById("ourContainer");
+
+ourContainer.innerHTML+=template;
+
+    
+
+}
+
+
+var a=document.getElementById("floatingTextarea");
+
+var i=window.localStorage.length;
+
+var ourBtn=document.getElementById("ourBtn");
+
+ourBtn.addEventListener('click',function(){
+
+    var valuee=a.value;
+    localStorage.setItem(`NOTE-${i}`,`${valuee}`);
+    i++;
+
+    
+var template=`<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title"></h5>
+  <p class="card-text">${valuee}</p>
+ 
+</div>
+</div>`
+
+    
+var ourContainer=document.getElementById("ourContainer");
+
+ourContainer.innerHTML+=template;
+
+console.log(valuee);
+
+})
